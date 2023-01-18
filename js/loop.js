131577f83ca2897094e12352ce5d628788db7671
naglismoc <dvd.filmas@gmail.com>
2023-01-18 18:47:35 +0200

    2d arr men women split

--- js/loop.js
+++ js/loop.js
@@ -40,7 +40,7 @@ for (let i = 10; i < 50; i++) {
 console.log("---------------");
 
 let i = 0;
-for ( ; true ; ) {
+for (; true;) {
     console.log(i);
     i++;
     if (i >= 10) {
@@ -49,20 +49,20 @@ for ( ; true ; ) {
 }
 console.log("---------------");
 
-for (let i = 0; i < 25; i+=4 ) {
+for (let i = 0; i < 25; i += 4) {
     console.log(i);
 }
 
-console.log("---------------");  
+console.log("---------------");
 
-for (let i = 24; i >= 0; i-- ) {
+for (let i = 24; i >= 0; i--) {
     console.log(i);
 }
 
 
 let sk = 0;
-            //  0               1           2       3       4       5       6
-let draugai = ['Saulius', 'Arnoldas', 'Jovita','Matas', 'Saulius', 'Arnas', 'Darius'];
+//  0               1           2       3       4       5       6
+let draugai = ['Saulius', 'Arnoldas', 'Jovita', 'Matas', 'Saulius', 'Arnas', 'Darius'];
 
 console.log(draugai);
 
@@ -83,7 +83,7 @@ for (let i = 0; i < draugai.length; i++) {
     console.log(draugai[i]);
 }
 
-console.log("---------------");  
+console.log("---------------");
 
 draugai[draugai.length] = "Ugnė";
 draugai[draugai.length] = "Ugnė";
@@ -100,9 +100,9 @@ for (let i = 0; i < draugai.length; i++) {
 for (let a = 1; a <= 10; a++) {
     let row = "";
     for (let i = 1; i <= 10; i++) {
-        if(a * i % 2 == 0){
-           row += "P ";
-        }else{
+        if (a * i % 2 == 0) {
+            row += "P ";
+        } else {
             row += "N ";
         }
         //row += a * i + " ";
@@ -112,7 +112,62 @@ for (let a = 1; a <= 10; a++) {
 
 
 let count = 0;
-for (let o = 0; o < 10; o++) {
-    count++;
+for (let o = 0; o < 20; o++) {
+    if (o % 2 == 0) {
+        count++;
+    }
 }
+
 console.log(count);
+
+// Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. Skaičiuojam abu atskirai.
+
+let count5 = 0;
+let count7 = 0;
+for (let i = 0; i < draugai.length; i++) {
+    if (draugai[i].length < 5) {
+        count5++;
+        continue;
+    }
+
+    if (draugai[i].length > 7) {
+        count7++;
+    }
+}
+
+console.log("Vardų trumpensių nei 5 simboliai yra " + count5, ", o ilgesnių nei 7 simboliai yra " + count7);
+
+let students = [["Ugnė", "moteris"], ["Saulius", "vyras"], ["Jurgis", "vyras"], ["Anastasija", "moteris"], ["Luka", "moteris"]];
+let countMen = 0;
+let countWomen = 0;
+for (let i = 0; i < students.length; i++) {
+    if (students[i][1] == "moteris") {
+        countWomen++;
+        continue;
+    }
+
+    if (students[i][1] == "vyras") {
+        countMen++;
+    }
+}
+
+console.log("Vyrų yra " + countMen, ", moterų yra " + countWomen);
+
+let arrWomen = [];
+let arrMen = [];
+for (let i = 0; i < students.length; i++) {
+    if (students[i][1] == "moteris") {
+        arrWomen.push(students[i][0]);
+        // arrWomen[i] = students[i][0];
+        continue;
+    }
+
+    if (students[i][1] == "vyras") {
+        arrMen.push(students[i][0]);
+        // arrMen[i] = students[i][0];
+
+    }
+}
+
+console.log(arrWomen);
+console.log(arrMen);
